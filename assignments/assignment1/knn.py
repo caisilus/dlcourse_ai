8a739f8a41d21bee2ef3382856b62a5fd0a2a5bf
caisilus <serangor@gmail.com>
2021-07-14 12:33:27 +0300

    first hometask

--- assignments/assignment1/knn.py
+++ assignments/assignment1/knn.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import queue
+import heapq
+from scipy import stats
 
 class KNN:
     """
@@ -37,6 +39,24 @@ class KNN:
         else:
             return self.predict_labels_multiclass(dists)
 
+
+    def l1_distance(self, v, u):
+        '''
+        Computes l1 distance between two vectors
+        '''
+        l1 = 0
+        for i in range(v.shape[0]):
+            l1 += abs(v[i] - u[i])
+        return l1
+
+
+    def l1_dist_matr(self, v, m):
+        u = np.sum(np.abs(m - v), axis=1)
+        #print(u)
+        #print(u.shape)
+        return u
+
+
     def compute_distances_two_loops(self, X):
         '''
         Computes L1 distance from every sample of X to every training sample
@@ -54,8 +74,10 @@ class KNN:
         dists = np.zeros((num_test, num_train), np.float32)
         for i_test in range(num_test):
             for i_train in range(num_train):
-                # TODO: Fill dists[i_test][i_train]
-                pass
+                # Fill dists[i_test][i_train]
+                dists[i_test][i_train] = self.l1_distance(self.train_X[i_train], X[i_test])
+        return dists
+
 
     def compute_distances_one_loop(self, X):
         '''
@@ -73,9 +95,13 @@ class KNN:
         num_test = X.shape[0]
         dists = np.zeros((num_test, num_train), np.float32)
         for i_test in range(num_test):
-            # TODO: Fill the whole row of dists[i_test]
+            # Fill the whole row of dists[i_test]
             # without additional loops or list comprehensions
-            pass
+            #vv = np.tile(X[i_test, :], (num_train, 1))
+            #print(vv.shape)
+            dists[i_test, :] = np.sum(np.abs(self.train_X - X[i_test, :]), axis=1)
+        return dists
+        
 
     def compute_distances_no_loops(self, X):
         '''
@@ -93,8 +119,13 @@ class KNN:
         num_test = X.shape[0]
         # Using float32 to to save memory - the default is float64
         dists = np.zeros((num_test, num_train), np.float32)
-        # TODO: Implement computing all distances with no loops!
-        pass
+        #Implement computing all distances with no loops!
+        dists = np.sum(np.abs(X[:,None] - self.train_X), axis=-1)
+        #dist_fun = np.vectorize(self.l1_dist_matr) #, signature='(n),(m)->(m)')
+        #dists = dist_fun(X, self.train_X)
+        
+        return dists
+
 
     def predict_labels_binary(self, dists):
         '''
@@ -108,12 +139,23 @@ class KNN:
         pred, np array of bool (num_test_samples) - binary predictions 
            for every test sample
         '''
+        def get_second(t):
+            return t[1]
+
         num_test = dists.shape[0]
         pred = np.zeros(num_test, np.bool)
         for i in range(num_test):
-            # TODO: Implement choosing best class based on k
+            # Implement choosing best class based on k
             # nearest training samples
-            pass
+            num_train = self.train_X.shape[0]
+            q = [(dists[i][j], self.train_y[j]) for j in range(num_train)]
+            arr = np.array(heapq.nsmallest(self.k, q, (lambda x: x[0])))
+            k_smallest_fun = np.vectorize(get_second, signature='(n)->()')
+            k_smallest = k_smallest_fun(arr)
+            mode = (stats.mode(k_smallest))[0] #most recent element
+            #print(bool(mode))
+            pred[i] = bool(mode)
+
         return pred
 
     def predict_labels_multiclass(self, dists):
@@ -128,11 +170,21 @@ class KNN:
         pred, np array of int (num_test_samples) - predicted class index 
            for every test sample
         '''
+
+        def get_second(t):
+            return t[1]
+
         num_test = dists.shape[0]
-        num_test = dists.shape[0]
+        num_train = dists.shape[1]
         pred = np.zeros(num_test, np.int)
         for i in range(num_test):
-            # TODO: Implement choosing best class based on k
+            # Implement choosing best class based on k
             # nearest training samples
-            pass
+            q = [(dists[i][j], self.train_y[j]) for j in range(num_train)]
+            arr = np.array(heapq.nsmallest(self.k, q, (lambda x: x[0])))
+            k_smallest_fun = np.vectorize(get_second, signature='(n)->()')
+            k_smallest = k_smallest_fun(arr)
+            mode = (stats.mode(k_smallest))[0]
+            pred[i] = int(mode)
+            #print(pred[i])
         return pred
--- assignments/assignment1/metrics.py
+++ assignments/assignment1/metrics.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def binary_classification_metrics(prediction, ground_truth):
     '''
     Computes metrics for binary classification
@@ -9,12 +11,39 @@ def binary_classification_metrics(prediction, ground_truth):
     Returns:
     precision, recall, f1, accuracy - classification metrics
     '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
 
-    # TODO: implement metrics!
+    #prediction
+    positives_predicted = np.count_nonzero(prediction)
+    negatives_predicted = np.count_nonzero(prediction == False)
+    count_all = prediction.shape[0]
+    assert(positives_predicted + negatives_predicted == count_all)
+
+    positives = np.count_nonzero(ground_truth) #relevant positives
+    negatives = np.count_nonzero(ground_truth == False) #relevant negatives
+    assert(positives + negatives == count_all)
+
+    
+    #print(prediction)
+    #print(ground_truth)
+
+
+    accurate_mask = (prediction == ground_truth)
+    accurates = np.count_nonzero(accurate_mask)
+    #print(accurate_mask)
+    #print(accurates)
+
+    true_positives_mask = accurate_mask == (accurate_mask <= prediction)
+    true_positives = np.count_nonzero(true_positives_mask)
+    #print(true_positives)
+    #print(positives_predicted)
+
+
+    precision = true_positives / positives_predicted
+    recall = true_positives / positives
+    accuracy = accurates / count_all
+    f1 = 2 * precision * recall / (precision + recall)
+
+    # implement metrics!
     # Some helpful links:
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
@@ -33,5 +62,10 @@ def multiclass_accuracy(prediction, ground_truth):
     Returns:
     accuracy - ratio of accurate predictions to total samples
     '''
-    # TODO: Implement computing accuracy
-    return 0
+    # Implement computing accuracy
+    count_all = prediction.shape[0]
+    accurate_mask = (prediction == ground_truth)
+    accurates = np.count_nonzero(accurate_mask)
+    accuracy = accurates / count_all
+
+    return accuracy
